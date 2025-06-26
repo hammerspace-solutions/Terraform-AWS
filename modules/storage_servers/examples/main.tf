2c00a5e1f8ff4944cf34a2db6c6edba64470fc7a
Michael Kade <mike@mikda.com>
2025-06-26 15:16:31 -0700

    Need to add a public ip to storage test so that ssh can get to the instance

--- modules/storage_servers/examples/main.tf
+++ modules/storage_servers/examples/main.tf
@@ -10,6 +10,10 @@ variable "project_name" {}
 variable "vpc_id" {}
 variable "subnet_id" {}
 variable "storage_ami" {}
+variable "ssh_public_key" {
+  description = "The public key material for the temporary SSH key."
+  type        = string
+}
 variable "storage_instance_type" {
   default = "t3.medium"
 }
@@ -26,16 +30,19 @@ variable "storage_raid_level" {
 variable "storage_user_data" {
   type = string
 }
-# ADDED: A variable to accept the public key material from the Go test
-variable "ssh_public_key" {
-  description = "The public key material for the temporary SSH key."
-  type        = string
-}
 
 # Create a temporary key pair for this test run
 resource "aws_key_pair" "test_key" {
   key_name   = "${var.project_name}-key"
   public_key = var.ssh_public_key
+}
+
+# An Elastic IP ensures the test instance has a reliable public IP address.
+resource "aws_eip" "test_eip" {
+  # We only need one EIP for our single test instance
+  count = var.storage_instance_count > 0 ? 1 : 0
+  
+  domain = "vpc"
 }
 
 # Look up the AZ from the provided subnet to ensure a match
@@ -47,18 +54,18 @@ data "aws_subnet" "test_subnet" {
 module "storage_servers" {
   source = "../" // Points to the storage_servers module root
 
-  # Pass variables to the module
   instance_count    = var.storage_instance_count
   ami               = var.storage_ami
   instance_type     = var.storage_instance_type
   raid_level        = var.storage_raid_level
   ebs_count         = var.storage_ebs_count
+  
   project_name      = var.project_name
   region            = var.region
   availability_zone = data.aws_subnet.test_subnet.availability_zone
   vpc_id            = var.vpc_id
   subnet_id         = var.subnet_id
-  key_name          = aws_key_pair.test_key.key_name # Use the newly created key pair
+  key_name          = aws_key_pair.test_key.key_name # Use the temporary key
   user_data         = var.storage_user_data
 
   # Use sensible defaults for other required variables
@@ -72,6 +79,15 @@ module "storage_servers" {
   capacity_reservation_id = null
 }
 
+# Associate the Elastic IP with our test instance after it has been created.
+resource "aws_eip_association" "test_eip_assoc" {
+  count = var.storage_instance_count > 0 ? 1 : 0
+
+  instance_id   = module.storage_servers.instance_details[0].id
+  allocation_id = aws_eip.test_eip[0].id
+}
+
+
 # Output the results for validation
 output "storage_instances" {
   description = "The details of the created storage instances."
@@ -83,8 +99,8 @@ output "region" {
   value       = var.region
 }
 
-# ADDED: Output the public IP for the SSH connection
+# Output the Elastic IP for the SSH connection
 output "public_ip" {
   description = "The public IP of the first storage server."
-  value       = module.storage_servers.instance_details[0].public_ip
+  value       = var.storage_instance_count > 0 ? aws_eip.test_eip[0].public_ip : null
 }
