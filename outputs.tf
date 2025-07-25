8263ba3e43be232b9c3d3b1e651a5cee1f74b187
Michael Kade <mike@mikda.com>
2025-07-25 07:51:01 -0700

    Update version number

--- outputs.tf
+++ outputs.tf
@@ -25,7 +25,7 @@
 
 output "terraform_project_version" {
   description = "The version of the Terraform-AWS project configuration."
-  value       = "2025.07.15-7ee91ea"
+  value       = "2025.07.24-a4a08b6"
 }
 
 output "bastion_details" {
