cc7ff8d9822ff1a059e204563c538ac3539a5cfc
Michael Kade <mike@mikda.com>
2025-10-24 12:54:24 -0700

    Change Terraform-AWS version

--- outputs.tf
+++ outputs.tf
@@ -25,7 +25,7 @@
 
 output "terraform_project_version" {
   description = "The version of the Terraform-AWS project configuration."
-  value       = "2025.08.04-033fe5b"
+  value       = "2025.10.24-655dbc7"
 }
 
 output "client_instances" {
